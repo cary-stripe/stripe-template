d46671d8432916cd1530a088962376b71bef1992
cary-stripe <cary@stripe.com>
2021-03-24 14:28:45 -0700

    initial stripe call

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/keys');
+const stripe = require('stripe')(config.STRPE_SK);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+
+  const charges = await stripe.charges.list({
+    limit: 3,
+  });
+
+  const firstCharge = charges.data[0];
+
+  res.render('index', {
+    title: 'Ecommerce', 
+    charge: firstCharge
+  });
 });
 
 module.exports = router;
